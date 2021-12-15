74c1c74ee567512814eebf550df65905a511182c
Kamil Mrowiec <kamil.mrowiec@iteo.com>
2021-12-15 22:10:35 +0100

    Moved assets to mobile

---
+++ mobile/lib/images.dart
@@ -0,0 +1,4 @@
+class ImagesPNG {
+  static const image1 = 'assets/images/image1.png';
+  static const image2 = 'assets/images/image2.png';
+}
--- presentation/lib/widgets/user_avatar.dart
+++ presentation/lib/widgets/user_avatar.dart
@@ -1,6 +1,7 @@
 import 'package:domain/users/data/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:mobile/images.dart';
 // import 'package:mobile/images.dart';
 
 class UserAvatar extends StatelessWidget {
@@ -12,8 +13,8 @@ class UserAvatar extends StatelessWidget {
     return ListTile(
       title: Row(
         children: [
-          Image.asset("assets/images/image1.png", height: 50, width: 50, package: 'presentation'),
-          Image.asset("assets/images/image2.png", height: 50, width: 50, package: 'presentation'),
+          Image.asset(ImagesPNG.image1, height: 50, width: 50),
+          Image.asset(ImagesPNG.image2, height: 50, width: 50),
           Text([user.name, user.home.latitude.toString(), user.home.longitude.toString()].join('    ')),
         ],
       ),
