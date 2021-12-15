fba5477e1b30a032af89cd69d7bf53c9d984a3ac
Kamil Mrowiec <kamil.mrowiec@iteo.com>
2021-12-15 21:32:32 +0100

    Removed unussed imports

--- presentation/lib/pages/users/users_page.dart
+++ presentation/lib/pages/users/users_page.dart
@@ -1,7 +1,5 @@
-import 'package:domain/users/data/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:latlng/latlng.dart';
 import 'package:presentation/custom_hooks/use_bloc.dart';
 import 'package:presentation/custom_hooks/use_bloc_listener.dart';
 import 'package:presentation/pages/users/users_cubit.dart';
