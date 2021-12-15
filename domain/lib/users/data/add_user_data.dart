fb9a90e522002cd7fe4f5f9ca6603f73eca6a4ed
Kamil Mrowiec <kamil.mrowiec@iteo.com>
2021-12-15 20:08:49 +0100

    Added domain first model and pubspec.yaml

---
+++ domain/lib/users/data/add_user_data.dart
@@ -0,0 +1,10 @@
+import 'package:latlng/latlng.dart';
+
+class AddUserData {
+  final String name;
+  final LatLng home;
+
+  AddUserData(this.name, this.home);
+}
+
+Map<String, dynamic> _noUsedDomainFunction() => <String, dynamic>{};
